27ffc6eedcb35d74381da6d1f096489c47e252ec
lvmemore <105576746+lvmemore@users.noreply.github.com>
2022-06-30 22:20:28 +0700

    Update Hardening_CentOS_BukanMeong.sh

--- Hardening_CentOS_BukanMeong.sh
+++ Hardening_CentOS_BukanMeong.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
-
-: '
-#SYNOPSIS
-    Quick win script for remediation of CentOS Linux 7 baseline misconfigurations.
-.DESCRIPTION
-    This script aims to remediate OS baseline misconfigurations for CentOS Linux 7 based Virtual machines on Azure.
-    **Total policies supported: 51
-
-.NOTES
-
-    Copyright (c) ZCSPM. All rights reserved.
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-    Version: 1.0
-    # PREREQUISITE
-
-.EXAMPLE
-    Ensure that you are logged in as root user. Use su command for the same.
-    Command to execute : bash Azure_CSBP_CentOS_Linux7_Remediation.sh
-.INPUTS
-
-.OUTPUTS
-    None
-'
+:
 
 RED='\033[1;31m'
 GREEN='\033[1;32m'
